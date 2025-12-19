93e16c5927ca6ed2c837bb2b80cba3c5ce8fed8d
danilo <danilo@Ubuntu.myguest.virtualbox.org>
2025-12-19 13:50:37 -0300

    Add cm to topic name

--- encoder_esp32/src/main.cpp
+++ encoder_esp32/src/main.cpp
@@ -107,7 +107,7 @@ void setup() {
             &publisher,
             &node,
             ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
-            "theter_odometry");
+            "theter_odometry_cm");
     }
 }
 
